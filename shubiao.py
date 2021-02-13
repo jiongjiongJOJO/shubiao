ca49f486581866beca5ee97606360393c6321c0e
囧囧JOJO <58727904+jiongjiongJOJO@users.noreply.github.com>
2021-02-13 08:26:36 +0800

    修改导入库的方式，大大降低打包后的文件大小

--- shubiao.py
+++ shubiao.py
@@ -1,21 +1,22 @@
-import pyautogui
+from pyautogui import position
+from pyautogui import moveTo
+from pyautogui import moveRel
 count = int(input ("请输入:"))
-currentMouseX, currentMouseY = pyautogui.position() # 鼠标当前位置
+currentMouseX, currentMouseY = position() # 鼠标当前位置
 print(currentMouseX, currentMouseY)
 while (count >= 0):
 # 控制鼠标移动,duration为持续时间
  for i in range(2):
-   pyautogui.moveTo(100, 100, duration=0.25) # 移动到 (100,100)
-   pyautogui.moveTo(200, 100, duration=0.25)
-   pyautogui.moveTo(200, 200, duration=0.25)
-   pyautogui.moveTo(100, 200, duration=0.25)
- 
+   moveTo(100, 100, duration=0.25) # 移动到 (100,100)
+   moveTo(200, 100, duration=0.25)
+   moveTo(200, 200, duration=0.25)
+   moveTo(100, 200, duration=0.25)
+  
  for i in range(2):
-   pyautogui.moveRel(50, 0, duration=0.25) # 从当前位置右移100像素
-   pyautogui.moveRel(0, 50, duration=0.25) # 向下
-   pyautogui.moveRel(-50, 0, duration=0.25) # 向左
-   pyautogui.moveRel(0, -50, duration=0.25) # 向上
+   moveRel(50, 0, duration=0.25) # 从当前位置右移100像素
+   moveRel(0, 50, duration=0.25) # 向下
+   moveRel(-50, 0, duration=0.25) # 向左
+   moveRel(0, -50, duration=0.25) # 向上
    count = count - 1
-
-print ('Good bye!')
  
+print ('Good bye!')
